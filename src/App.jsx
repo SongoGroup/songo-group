d95c5dfa6c6a4596b7656a7c8b75afac91f97062
iqbalmuhammadd26 <iqbalmuhammadd26@gmail.com>
2024-09-25 17:19:20 +0000

    first init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <Home/>
+    </>
+
+  )
+}
